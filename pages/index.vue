6e789f2ffe0b1e027a510f6d73bedcdee7307541
renoinn <oomori.lunch@gmail.com>
2023-03-14 15:03:29 +0900

    remove public setting

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { Content } from '@/server/types'
-import { MicroCMSListResponse } from 'microcms-js-sdk'
 
-//const { data: posts } = await useFetch<MicroCMSListResponse<Content>>('/api/content_list')
 const { data, pending, error, refresh } = await useAsyncData('contents', () => {
     return getClient().getList<Content>({
         endpoint: 'blogs',
