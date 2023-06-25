942809616f12c674af8ad84cf9e8b174cc01628c
renoinn <oomori.lunch@gmail.com>
2023-06-25 15:35:43 +0900

    fix cloudfront

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,6 +5,7 @@ import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from 'constructs';
 
 export class CdkStack extends cdk.Stack {
@@ -28,14 +29,31 @@ export class CdkStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
+    const cfOriginAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      "cfOriginAccessIdentity",
+      {}
+    );
+    const cloudfrontS3Access = new iam.PolicyStatement();
+    cloudfrontS3Access.addActions("s3:GetBucket*");
+    cloudfrontS3Access.addActions("s3:GetObject*");
+    cloudfrontS3Access.addActions("s3:List*");
+    cloudfrontS3Access.addResources(bucket.bucketArn);
+    cloudfrontS3Access.addResources(`${bucket.bucketArn}/*`);
+    cloudfrontS3Access.addCanonicalUserPrincipal(
+      cfOriginAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    );
+    bucket.addToResourcePolicy(cloudfrontS3Access);
+
     //const s3Origin = new origins.S3Origin(bucket);
     // CloudFrontを設定
     const distribution = new cloudfront.CloudFrontWebDistribution(this, "nuxtBlogDistribution", {
+      defaultRootObject: "",
       originConfigs: [
         {
           s3OriginSource: {
             s3BucketSource: bucket,
-
+            originAccessIdentity: cfOriginAccessIdentity,
           },
           behaviors: [
             {
